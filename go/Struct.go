70427fd54c801d510431bb2e4612a9c27c087ab7
arttur <artur.poetsch@ynov.com>
2022-03-24 16:36:10 +0100

    oui

--- go/Struct.go
+++ go/Struct.go
@@ -1,21 +1,35 @@
 package main
 
 type Utilisateurs struct {
-<<<<<<< HEAD
 	Id_utilisateurs int
 	Nom             string
 	Prenom          string
 	Adresse_mail    string
 	Pseudo          string
-=======
-	Id_utilisateurs		int
-	Nom       			string
-	Pseudo				string
-	Prenom				string
-	Adresse_mail		string
->>>>>>> 15daca9b751687eafe3fb314adef25b4f7557c4f
 }
 
 type Stock_forum struct {
-	Id_user int
+	Id_user      int
+	Commentaires string
+	User_level   string
+	Upvotes      int
+	Downvotes    int
+}
+
+type Adresses struct {
+	Ville       string
+	Id_data     int
+	Code_postal string
+}
+
+type services struct {
+	Logs    string
+	Adress  string
+	Id_logs int
+}
+
+type gestion struct {
+	Admin_id   int
+	Admin_name string
+	Modo_name  string
 }
