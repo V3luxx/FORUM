3a6ac3138135b1ff4301cb480356d1574d9bf319
arttur <artur.poetsch@ynov.com>
2022-03-28 09:40:46 +0200

    oui

---
+++ script/connect.php
@@ -0,0 +1,9 @@
+<?php
+    $login = $_POST['pseudo'];
+    $password = $_POST['mot_de_passe'];
+    $firstname = $_POST['prenom'];
+    $secondname = $_POST['nom'];
+    $mail = $_POST['adresse_mail'];
+
+    $conn = new mysqli('localhost', 'root', )
+?>
