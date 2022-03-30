1c14779049be02eba6e9bd3c359a1b3cd355b62b
arttur <artur.poetsch@ynov.com>
2022-03-30 10:29:22 +0200

    oui

---
+++ go/insert.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func Insert() []Utilisateurs {
+
+	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/forum")
+
+	if err != nil {
+		log.Fatal(err)
+		defer db.Close()
+	}
+	var user []Utilisateurs
+	res, err := db.Query("INSERT INTO Utilisateurs Values(", )
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for res.Next() {
+		var oneUser Utilisateurs
+		err := res.Scan(&oneUser.Id_utilisateurs, &oneUser.Nom, &oneUser.Pseudo, &oneUser.Prenom, &oneUser.Adresse_mail, &oneUser.Mot_de_passe)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+		user = append(user, oneUser)
+		/* fmt.Printf("%v\n", u) */
+	}
+	defer res.Close()
+	return user
+}
